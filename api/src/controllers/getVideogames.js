f24dcb271136b04054f3b1c01511df1cbc9f3a19
Marianoromero8 <marianoromero97.mr@gmail.com>
2024-06-13 18:56:09 -0300

    Controller getVideogames done

--- api/src/controllers/getVideogames.js
+++ api/src/controllers/getVideogames.js
@@ -1,7 +1,66 @@
+const axios = require('axios');
+require('dotenv').config();
+const {API_KEY} = process.env;
+const{Videogames, Genres} = require('../db')
 
+const getVideogames = async (req, res) => {
+    try{
+        const response = await axios(`https://api.rawg.io/api/games?key=${API_KEY}`)
 
-const getVideogames = () => {
+        const {results} = response.data;
 
+        return results.map((videogame) => {
+            const image = videogame.background_image;
+            const description = videogame.reviews_text_count;
+            const platforms = videogame.platforms;
+            const released = videogame.released;
+            const rating = videogame.rating;
+
+            return({
+                id: videogame.id,
+                name: videogame.name,
+                image,
+                description,
+                platforms,
+                released,
+                rating
+            })
+        })
+    }
+    catch(error){
+        throw new Error(error.message)
+    }
 }
+    const getVideogamesFromDB = async (req, res) => {
+        try{
+            const vg = await Videogames.findAll({
+                include: {
+                    model: Genres,
+                    attributes: ['name'],
+                    through: {
+                        attributes: []
+                    }
+                }
+            })
+            return(vg)
+        }
+        catch(error){
+            return({error: error.message})
+        }
+    }
 
-module.exports = { getVideogames };
+    const allVideogames = async (req, res) => {
+        try{
+            const apiVideogames = await getVideogames();
+            const dbVideogames = await getVideogamesFromDB();
+
+            const callVideogames = [...apiVideogames, ...dbVideogames]
+
+            res.status(200).json(callVideogames)
+        }
+        catch(error){
+            res.status(400).json({error: "No hay nada para llamar"})
+        }
+    }
+
+module.exports = { allVideogames };
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { getVideogames } = require('../controllers/getVideogames');
+const { allVideogames } = require('../controllers/getVideogames');
 const { getId } = require('../controllers/getId');
 const { getName } = require('../controllers/getName');
 const { postVideogames } = require('../controllers/postVideogames')
 
-router.get("/", getVideogames)
+router.get("/", allVideogames)
 router.get("/:id", getId)
 router.get("/videogame/:name", getName)
 router.post("/post", postVideogames)
