3e2c0525b58fc8d0c60ea4508f3d456e75118f1d
Marianoromero8 <marianoromero97.mr@gmail.com>
2024-06-21 17:55:25 -0300

    getvg and getdetail done

--- api/src/controllers/getId.js
+++ api/src/controllers/getId.js
@@ -1,7 +1,43 @@
 const axios = require('axios')
+const {API_KEY} = process.env;
 
-const getId = () => {
+const getId = async (id) => {
+    try{
+    const videogameIdApi = await axios (`https://api.rawg.io/api/games/${id}?key=${API_KEY}`);
 
+    const {data} = videogameIdApi;
+
+    const dataById = {
+        id: data.id,
+        name: data.name,
+        image: data.background_image,
+        platform: data.platforms.map(plat => plat.platform.name),
+        description: data.reddit_description,
+        released: data.released,
+        rating: data.rating,
+        genres: data.genres.map(genre => genre.name)
+    }
+
+    return dataById;
+    }
+    catch(error){
+        throw new Error('Details Not Found')
+    }
 }
 
-module.exports = { getId };
+const getDetail = async (req, res) => {
+    const {id} = req.params;
+    try{
+        const videogame = await getId(id);
+
+        return res.status(200).json(videogame);
+    }
+    catch(error){
+        if(error.message){
+            return res.status(400).send('Videogame Not Found')
+        }
+        res.status(error.statusCode || 500).json(`error interno - ${error.message}`)
+    }
+}
+
+module.exports = { getDetail };
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const { allVideogames } = require('../controllers/getVideogames');
-const { getId } = require('../controllers/getId');
+const { getDetail } = require('../controllers/getId');
 const { getName } = require('../controllers/getName');
 const { postVideogames } = require('../controllers/postVideogames')
 
 router.get("/", allVideogames)
-router.get("/:id", getId)
+router.get("/:id", getDetail)
 router.get("/videogame/:name", getName)
 router.post("/post", postVideogames)
 
